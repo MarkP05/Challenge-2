95f5c7bd0fe25600d77f9bbffd81db69e05fad1c
MarkP05 <mvpfister5@gmail.com>
2025-04-16 18:47:25 -0400

    Template setup

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.err.println("Hello, World!");
+public class Main {
+    public static void main(String[] args) {
+        System.err.println("Hello, World!");
+    }
 }
