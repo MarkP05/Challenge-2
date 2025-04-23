d59f7aa7b2b9ae99b1facdbe29c053ed12b9e15d
reZdotjson <0083rei@proton.me>
2025-04-23 18:29:14 -0400

    push 2

--- Function.java
+++ Function.java
@@ -1,3 +1,26 @@
+import git.tools.client.GitSubprocessClient;
+import github.tools.client.GitHubApiClient;
+import github.tools.client.RequestParams;
+import github.tools.responseObjects.CreateRepoResponse;
+import github.tools.responseObjects.GetRepoInfoResponse;
+
 public class Function {
-    
+    public static void main(String[] args) {
+        // GitHubApiClient gitHubApiClient = new GitHubApiClient(user, token);
+        // GetRepoInfoResponse repoInfo = gitHubApiClient.getRepoInfo("CSC109", "GitHubApiClient");
+        // System.out.println(repoInfo.getDescription());
+
+        String repoPath = "C:/Users/Rei/Documents/Homepage/colleg year 1/2-CSC109/week 12/Challenge-2";
+
+        //GitSubprocessClient gitSubprocessClient = new GitSubprocessClient(repoPath);
+
+
+        //GitHubApiClient.
+        
+        // List<String> lines = Files.readAllLines(Paths.get("WB2EB.txt"));
+        // int randomIndex = (int) (Math.random() * lines.size());
+        // String randomString = lines.get(randomIndex);
+        // int string_counter = randomString.length();
+
+    }
 }
